4f431ba7dec603438a6602d0ee0c03a19191d8d1
Danielle Hu <daniellh@andrew.cmu.edu>
2015-07-28 22:37:59 -0400

    working on that desktop ux doeeee

--- bobsBurgers.js
+++ bobsBurgers.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
   $("header").css("height", innerH*0.32);
   $("#bottom").css("height", innerH*0.68);
   $("#about").css("height", innerH*0.35);
+  $("#about_box").css("height", innerH*0.35);
+  $("#project_box").css("height", innerH * 0.35 * .483);
+  $("#experience_box").css("height", innerH * 0.35 * .483);
+  $("#hobbies_box").css("height", innerH*0.17);
   $("#projExp").css("height", innerH*0.35);
   $("#hobbies").css("height", innerH*0.17);
-    
 /* jQuery.data(window, "size", innerW);
 
   function resizeFont(deltaVal) {
@@ -31,5 +34,31 @@ $(document).ready(function() {
   });
 */    
     
+    if ($(window).innerWidth() > 750) {
+        $("#about").mouseenter(function () {
+            $("#about_box").slideDown("fast");
+        });
+        $("#about").mouseleave(function() {
+            $("#about_box").slideUp("fast");
+    });
+        $("#projects").mouseenter(function () {
+            $("#project_box").slideDown("fast");
+        });
+        $("#projects").mouseleave(function() {
+            $("#project_box").slideUp("fast");
+    });
+        $("#experiences").mouseenter(function () {
+            $("#experience_box").slideDown("fast");
+        });
+        $("#experiences").mouseleave(function() {
+            $("#experience_box").slideUp("fast");
+    });
+        $("#hobbies").mouseenter(function () {
+            $("#hobbies_box").slideDown("fast");
+        });
+        $("#hobbies").mouseleave(function() {
+            $("#hobbies_box").slideUp("fast");
+    });
+    };
 });
     
