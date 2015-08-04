58b6fb1088bf8363717c72888f1f08b380ff9643
Danielle Hu <daniellh@andrew.cmu.edu>
2015-08-04 19:49:43 -0400

    desktop should be fine, mobiles need a lot of work, nav button issues test

--- bobsBurgers.js
+++ bobsBurgers.js
@@ -10,34 +10,11 @@ $(document).ready(function() {
   $("#hobbies_box").css("height", innerH*0.17);
   $("#projExp").css("height", innerH*0.35);
   $("#hobbies").css("height", innerH*0.17);
-/* jQuery.data(window, "size", innerW);
 
-  function resizeFont(deltaVal) {
-    var fontSize = $("#projExp").css("fontSize");
-    $("#projExp").css("fontSize", 
-    parseFloat(fontSize, 10) + parseFloat(deltaVal, 10));
-  }
-
-  $(window).resize(function() {
-    if ($(window).innerWidth() > 750) {
-          if ($(window).innerWidth() < jQuery.data(window, "size")) {
-              resizeFont(-0.2);
-          }
-          else if ($(window).innerWidth() == window.screen.width) { 
-              $("#projExp").css("fontSize", "500%"); 
-          }
-          else if ($(window).innerWidth() > jQuery.data(window, "size")) { 
-              resizeFont(0.2); 
-          }
-          jQuery.data(window, "size", $(window).innerWidth());
-    };
-  });
-*/    
-    
-    if ($(window).innerWidth() > 750) {
+    if ($(window).innerWidth() > 992) {
         $("#about").mouseenter(function () {
             $("#about_box").slideDown("fast");
-        });
+            });
         $("#about").mouseleave(function() {
             $("#about_box").slideUp("fast");
     });
